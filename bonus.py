093fe2001dc4ee270803a0483f1c7925a3537a64
ria4232 <ipkovalenomaria@gmail.com>
2024-05-26 00:56:18 +0300

    hi1

--- bonus.py
+++ bonus.py
@@ -3,6 +3,7 @@ import streamlit as st
 def show():
     video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     if video_url:
+        #comit
         col1, col2, col3 = st.columns(3)
         for col in (col1, col2, col3):
             with col:
